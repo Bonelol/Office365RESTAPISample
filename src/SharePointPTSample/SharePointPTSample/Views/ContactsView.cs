bb2b891ed96a6c1e3dd3b8271fe28b55a2d62c43
Sara Silva <saramgsilva@gmail.com>
2014-07-07 13:05:54 +0100

    Fixes toolbar for Contactview.

--- src/SharePointPTSample/SharePointPTSample/Views/ContactsView.cs
+++ src/SharePointPTSample/SharePointPTSample/Views/ContactsView.cs
@@ -14,63 +14,64 @@ namespace SharePointPTSample.Views
         private bool _iscreating;
         public ContactsView()
         {
-            _viewModel = App.Locator.ContactsViewModel;
-            BindingContext = _viewModel;
-            _viewModel.PropertyChanged += _viewModel_PropertyChanged;
-            _viewModel.LoadAsync();
-            _iscreating = true;
-            Title = "Contacts";
-            Label title = null;
-            if (Device.OS == TargetPlatform.WinPhone)
+            try
             {
-                title = new Label
+                _viewModel = App.Locator.ContactsViewModel;
+                BindingContext = _viewModel;
+                _viewModel.PropertyChanged += _viewModel_PropertyChanged;
+                _viewModel.LoadAsync();
+                _iscreating = true;
+                Title = "Contacts";
+                Label title = null;
+                if (Device.OS == TargetPlatform.WinPhone)
                 {
-                    Text = Title,
-                    XAlign = TextAlignment.Center,
-                    Font = Font.SystemFontOfSize(42)
+                    title = new Label
+                    {
+                        Text = Title,
+                        XAlign = TextAlignment.Center,
+                        Font = Font.SystemFontOfSize(42)
+                    };
+                }
+                NavigationPage.SetHasNavigationBar(this, true);
+
+                _listView = new ListView
+                {
+                    RowHeight = 40,
+                    ItemTemplate = new DataTemplate(typeof(ContactItemCell))
                 };
-            }
-            NavigationPage.SetHasNavigationBar(this, true);
+                _listView.SetBinding(ListView.ItemsSourceProperty, "Contacts");
+                _listView.ItemSelected += async (sender, e) =>
+                {
+                    if (_listView.SelectedItem != null)
+                    {
+                        await EditContact((Contact)e.SelectedItem);
+                    }
+                    _listView.SelectedItem = null;
+                };
 
-            _listView = new ListView
-            {
-                RowHeight = 40,
-                ItemTemplate = new DataTemplate(typeof (ContactItemCell))
-            };
-            _listView.SetBinding(ListView.ItemsSourceProperty, "Contacts");
-            _listView.ItemSelected += async (sender, e) => await EditContact((Contact)e.SelectedItem);
-
-            // the root control
-            var stackPanel = new StackLayout
-            {
-                VerticalOptions = LayoutOptions.FillAndExpand,
-                Children =
+                // the root control
+                var stackPanel = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.FillAndExpand,
+                    Children =
                 {
                     _listView
                 }
-            };
+                };
 
-            // add title for WP
-            if (Device.OS == TargetPlatform.WinPhone)
-            {
-                stackPanel.Children.Insert(0, title);
+                // add title for WP
+                if (Device.OS == TargetPlatform.WinPhone)
+                {
+                    stackPanel.Children.Insert(0, title);
+                }
+
+                Content = stackPanel;
             }
-
-            Content = stackPanel;
-
-            var tbi = new ToolbarItem("-", null, () => EditContact(null), 0, 0);
-
-            if (Device.OS == TargetPlatform.Android)
+            catch (Exception exception)
             {
-                // BUG: Android doesn't support the icon being null
-                tbi = new ToolbarItem("+", "plus", () => EditContact(null), 0, 0);
+                // todo handle the error
+                var error = exception.ToString();
             }
-            if (Device.OS == TargetPlatform.WinPhone)
-            {
-                // the image must be set as content and copy always
-                tbi = new ToolbarItem("add", "/Toolkit.Content/ApplicationBar.Add.png", () => EditContact(null), 0, 0);
-            }
-            ToolbarItems.Add(tbi);
         }
 
         private void _viewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
@@ -87,17 +88,61 @@ namespace SharePointPTSample.Views
             await Navigation.PushAsync(editContactView);
         }
 
+        private void CreatToolbarItems()
+        {
+            var tbi = new ToolbarItem("+", null, async () =>
+            {
+                await EditContact(new Contact());
+            }, 0, 0);
+
+
+            if (Device.OS == TargetPlatform.Android)
+            { // BUG: Android doesn't support the icon being null
+                tbi = new ToolbarItem("+", "plus", async () =>
+                {
+                    await EditContact(new Contact());
+                }, 0, 0);
+            }
+            if (Device.OS == TargetPlatform.WinPhone)
+            {
+                // the image must be set as content and copy always
+                tbi = new ToolbarItem("add", "/Toolkit.Content/ApplicationBar.Add.png", async () =>
+                {
+                    await EditContact(new Contact());
+                }, 0, 0);
+
+            }
+            ToolbarItems.Add(tbi);
+        }
+
+        protected override void OnDisappearing()
+        {
+            ToolbarItems.Clear();
+            base.OnDisappearing();
+        }
+
         protected override void OnAppearing()
         {
+           
             base.OnAppearing();
-            if (_iscreating)
+
+            try
             {
-                //in this case i called the LoadAsync in constructor
-                _iscreating = false;
+                if (_iscreating)
+                {
+                    //in this case i called the LoadAsync in constructor
+                    _iscreating = false;
+                }
+                else
+                {
+                    _viewModel.LoadAsync();
+                }
+                CreatToolbarItems();
             }
-            else
-            {
-                _viewModel.LoadAsync();
+            catch (Exception exception)
+            {   
+                // todo handle the error
+                var error = exception.ToString();
             }
         }
     }
