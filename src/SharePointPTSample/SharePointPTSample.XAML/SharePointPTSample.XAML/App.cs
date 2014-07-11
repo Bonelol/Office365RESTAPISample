1efaa1d924fa1d9e823f06b4b7200ec1346c91fc
Sara Silva <saramgsilva@gmail.com>
2014-07-11 17:28:11 +0100

    Add project for xaml sample.

---
+++ src/SharePointPTSample/SharePointPTSample.XAML/SharePointPTSample.XAML/App.cs
@@ -0,0 +1,24 @@
+﻿using SharePointPTSample.ViewModels;
+using SharePointPTSample.Views;
+using Xamarin.Forms;
+
+namespace SharePointPTSample.XAML
+{
+    public class App
+    {
+        private static ViewModelLocator _locator;
+
+        public static ViewModelLocator Locator
+        {
+            get
+            {
+                return _locator ?? (_locator = new ViewModelLocator());
+            }
+        }
+
+        public static Page GetMainPage()
+        {
+            return new NavigationPage(new LoginView());
+        }
+    }
+}
---
+++ src/SharePointPTSample/SharePointPTSample.XAML/SharePointPTSample.XAML/Views/EditContactView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharePointPTSample.Views
+{
+    public partial class EditContactView
+    {
+        public EditContactView()
+        {
+            InitializeComponent();
+        }
+    }
+}
